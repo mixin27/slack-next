c77a3993fce78d68dd309feb5f8c378f44d76d6a
mixin27 <kyawzayartun.dev@gmail.com>
2024-10-03 22:06:01 +0630

    emoji picker component

--- src/components/global/editor/index.tsx
+++ src/components/global/editor/index.tsx
@@ -15,6 +15,7 @@ import Hint from "../hint";
 import { cn } from "@/lib/utils";
 
 import "quill/dist/quill.snow.css";
+import EmojiPopover from "../emoji-popover";
 
 type EditorValue = {
   image: File | null;
@@ -134,6 +135,13 @@ const Editor = ({
     }
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onEmojiSelect = (emoji: any) => {
+    const quill = quillRef.current;
+
+    quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
+  };
+
   const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
@@ -155,16 +163,11 @@ const Editor = ({
             </Button>
           </Hint>
 
-          <Hint label="Emoji">
-            <Button
-              disabled={disabled}
-              size="iconSm"
-              variant="ghost"
-              onClick={() => {}}
-            >
+          <EmojiPopover onEmojiSelect={onEmojiSelect}>
+            <Button disabled={disabled} size="iconSm" variant="ghost">
               <Smile className="size-4" />
             </Button>
-          </Hint>
+          </EmojiPopover>
 
           {variant === "create" && (
             <Hint label="Image">
@@ -216,11 +219,18 @@ const Editor = ({
         </div>
       </div>
 
-      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
-        <p>
-          <strong>Shift + Return</strong> to add a new line
-        </p>
-      </div>
+      {variant === "create" && (
+        <div
+          className={cn(
+            "p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition",
+            !isEmpty && "opacity-100"
+          )}
+        >
+          <p>
+            <strong>Shift + Return</strong> to add a new line
+          </p>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/global/emoji-popover/index.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+type Props = {
+  children: React.ReactNode;
+  hint?: string;
+  onEmojiSelect: (emoji: unknown) => void;
+};
+
+const EmojiPopover = ({ children, hint = "Emoji", onEmojiSelect }: Props) => {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSelect = (emoji: any) => {
+    onEmojiSelect(emoji);
+    setPopoverOpen(false);
+
+    setTimeout(() => {
+      setTooltipOpen(false);
+    }, 500);
+  };
+
+  return (
+    <TooltipProvider>
+      <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
+        <Tooltip
+          open={tooltipOpen}
+          onOpenChange={setTooltipOpen}
+          delayDuration={50}
+        >
+          <PopoverTrigger asChild>
+            <TooltipTrigger asChild>{children}</TooltipTrigger>
+          </PopoverTrigger>
+          <TooltipContent className="bg-black text-white border border-white/5">
+            <p className="font-medium text-xs">{hint}</p>
+          </TooltipContent>
+        </Tooltip>
+
+        <PopoverContent className="p-0 w-full border-none shadow-none">
+          <Picker data={data} onEmojiSelect={onSelect} />
+        </PopoverContent>
+      </Popover>
+    </TooltipProvider>
+  );
+};
+
+export default EmojiPopover;
