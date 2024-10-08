0d3de62091a8851afc678397e7508c7bf479d97d
mixin27 <kyawzayartun.dev@gmail.com>
2024-10-08 09:05:03 +0630

    add loader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,8 @@ export default function Home() {
   }, [isLoading, workspaceId, open, setOpen, router]);
 
   return (
-    <div>
-      <UserButton />
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground" />
     </div>
   );
 }
